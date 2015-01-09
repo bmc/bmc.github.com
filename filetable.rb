558093ebf4da75e27d46381bcad9e112e4da453b
Brian Clapper <bmc@clapper.org>
2015-01-09 15:01:58 -0500

    Updated Ruby version. Fixed handling of embedded Markdown in software.yaml.

--- filetable.rb
+++ filetable.rb
@@ -1,91 +1,95 @@
 require 'yaml'
+require 'kramdown'
 
 class HTMLFromYAML
 
-    def initialize(input_yaml_file, output_html_file, options = {})
-        @yaml = input_yaml_file
-        @output_file = output_html_file
-        @css_prefix = options.fetch('css_prefix', 'file_table')
-        @table_css = "#{@css_prefix}_table"
-        @subheading_css = "#{@css_prefix}_header"
-        @entry_css = "#{@css_prefix}_entry"
-        @blurb_css = "#{@css_prefix}_blurb"
-        @desc_css = "#{@css_prefix}_desc"
+  def initialize(input_yaml_file, output_html_file, options = {})
+    @yaml = input_yaml_file
+    @output_file = output_html_file
+    @css_prefix = options.fetch('css_prefix', 'file_table')
+    @table_css = "#{@css_prefix}_table"
+    @subheading_css = "#{@css_prefix}_header"
+    @entry_css = "#{@css_prefix}_entry"
+    @blurb_css = "#{@css_prefix}_blurb"
+    @desc_css = "#{@css_prefix}_desc"
+  end
+
+  def make
+    puts("Making #{@output_file}")
+    if not File.exists?(@yaml)
+      error "Can't find file #{@yaml} in \\_includes directory."
     end
 
-    def make
-        puts("Making #{@output_file}")
-        if not File.exists?(@yaml)
-            error "Can't find file #{@yaml} in \\_includes directory."
-        end
-
-        begin
-            make_output_html
-        rescue
-            if (File.exists? @output_file)
-                File.delete @output_file
-            end
-        end
+    begin
+      make_output_html
+    rescue
+      if (File.exists? @output_file)
+        File.delete @output_file
+      end
     end
+  end
 
-    private
+  private
 
-    def make_output_html
-        File.open(@output_file, 'w') do |out |
-            out << "<table markdown='1' class='#{@table_css}'>"
+  def make_output_html
+    File.open(@output_file, 'w') do |out|
+      out << "<table markdown='1' class='#{@table_css}'>"
 
-            data = YAML.load(File.open(@yaml, 'r'))
-            if not data.is_a? Array
-                error "#{@yaml}: Expected sequence of sections."
-            end
+      data = YAML.load(File.open(@yaml, 'r'))
+      if not data.is_a? Array
+        error "#{@yaml}: Expected sequence of sections."
+      end
 
-            t = 0
-            data.each do |section|
-                t += 1
-                topic = section['topic'] or
-                    error "Missing 'topic' in section #{t}"
+      t = 0
+      data.each do |section|
+        t += 1
+        topic = section['topic'] or
+          error "Missing 'topic' in section #{t}"
 
-                out << sub_heading(topic)
+        out << sub_heading(topic)
 
-                items = section['items'] or
-                    error("Missing list items for topic '#{topic}'")
-                if not items.is_a? Array
-                    error("Items for topic '#{topic}' is not a list")
-                end
-
-                i = 0
-                items.each do |item|
-                    i += 1
-                    error_prefix = "Topic '#{topic}', item #{i}"
-                    url = item['url'] or error("#{error_prefix}: No URL")
-                    blurb = item['blurb']
-                    blurb_img = item['img']
-                    if not blurb.nil?
-                        # Use blurb
-                    elsif not blurb_img.nil?
-                        blurb = "![#{blurb_img}](#{blurb_img})"
-                    else
-                        error "#{error_prefix}: No 'blurb' or 'img'."
-                    end
-                    desc = item['description'] or
-                        error"#{error_prefix}: No description"
-                    out << entry(blurb, url, desc)
-                end
-            end
-            out << '</table>'
+        items = section['items'] or error("No items for topic '#{topic}'")
+        if not items.is_a? Array
+          error("Items for topic '#{topic}' is not a list")
         end
-    end
 
-    def sub_heading(text)
-        %{
+        i = 0
+        items.each do |item|
+          i += 1
+          error_prefix = "Topic '#{topic}', item #{i}"
+          url = item['url'] or error("#{error_prefix}: No URL")
+          blurb = item['blurb']
+          blurb_img = item['img']
+          if not blurb.nil?
+            blurb = markdown2html(blurb)
+          elsif not blurb_img.nil?
+            blurb = "![#{blurb_img}](#{blurb_img})"
+          else
+            error "#{error_prefix}: No 'blurb' or 'img'."
+          end
+          desc = item['description'] or error"#{error_prefix}: No description"
+          desc = markdown2html(desc)
+          out << entry(blurb, url, desc)
+        end
+      end
+      out << '</table>'
+    end
+  end
+
+  def sub_heading(text)
+    %{
   <tr class="#{@subheading_css}">
     <td align="center" colspan="2">#{text}</td>
   </tr>
 }
-    end
+  end
 
-    def entry(blurb, link, desc)
-        %{
+  def markdown2html(s)
+    Kramdown::Document.new(s).to_html
+  end
+
+  def entry(blurb, link, desc)
+    %{
   <tr class="#{@entry_css}">
     <td class="#{@blurb_css}">
       <a href="#{link}">#{blurb}</a>
@@ -93,9 +97,9 @@ class HTMLFromYAML
     <td class="#{@desc_css}">#{desc}</td>
   </tr>
 }
-    end
+  end
 
-    def error(msg)
-        raise RuntimeException, msg
-    end
+  def error(msg)
+    raise RuntimeException, msg
+  end
 end
